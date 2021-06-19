fa8098f2c62bd2cfd2a0cba57cf5fd6373638bb1
Thilin <fernando_cauper@hotmail.com>
2021-06-19 17:30:53 -0400

    first commit

---
+++ src/main/java/com/mxhstudio/pvpstatswotv/PvpstatswotvApplication.java
@@ -0,0 +1,13 @@
+package com.mxhstudio.pvpstatswotv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvpstatswotvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvpstatswotvApplication.class, args);
+	}
+
+}
