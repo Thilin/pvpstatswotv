02e685f28e560fa484eb9acd54e5049f7db3f207
Thilin <fernando_cauper@hotmail.com>
2021-07-19 20:08:51 -0400

    refactor: better documentation for visioncard controller

--- src/main/java/com/mxhstudio/pvpstatswotv/controller/VisionCardController.java
+++ src/main/java/com/mxhstudio/pvpstatswotv/controller/VisionCardController.java
@@ -2,6 +2,8 @@ package com.mxhstudio.pvpstatswotv.controller;
 
 import com.mxhstudio.pvpstatswotv.dto.VisionCardResponseDTO;
 import com.mxhstudio.pvpstatswotv.service.VisionCardService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +21,18 @@ public class VisionCardController {
         this.visionCardService = visionCardService;
     }
 
-    @GetMapping(value = "/{id}")
-    public VisionCardResponseDTO findById(@PathVariable Long id){
-        return visionCardService.findById(id);
+    @GetMapping(value = "/{id}", produces = "application/json")
+    @Operation(summary = "Find VisionCard", description = "Find a VisionCard using its id")
+    public ResponseEntity<VisionCardResponseDTO> findById(@PathVariable Long id){
+        var dto = visionCardService.findById(id);
+        if(dto == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().body(visionCardService.findById(id));
     }
 
-    @GetMapping(value = "/all")
-    public List<VisionCardResponseDTO> listAll(){
-        return visionCardService.listAll();
+    @GetMapping(value = "/all", produces = "application/json")
+    @Operation(summary = "List all Vision cards", description = "List all vision cards with their infos")
+    public ResponseEntity<List<VisionCardResponseDTO>> listAll(){
+        return ResponseEntity.ok().body(visionCardService.listAll());
     }
 }
--- src/main/java/com/mxhstudio/pvpstatswotv/dto/VisionCardResponseDTO.java
+++ src/main/java/com/mxhstudio/pvpstatswotv/dto/VisionCardResponseDTO.java
@@ -1,5 +1,6 @@
 package com.mxhstudio.pvpstatswotv.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class VisionCardResponseDTO {
 
+    @Schema( description = "Vision card id", example = "1")
     private  Long id;
+    @Schema(description = "Vision card description", example = "Ifrit")
     private  String description;
+    @Schema(description = "Vision card image", example = "Ifrit")
     private  String image;
 }
