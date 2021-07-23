c4941f27e8db45db20fe70e62008d39d6be30f6a
Thilin <fernando_cauper@hotmail.com>
2021-07-23 18:52:28 -0400

    refactor: added confrontationcontroller documentation

--- src/main/java/com/mxhstudio/pvpstatswotv/controller/ConfrontationController.java
+++ src/main/java/com/mxhstudio/pvpstatswotv/controller/ConfrontationController.java
@@ -2,10 +2,13 @@ package com.mxhstudio.pvpstatswotv.controller;
 
 import com.mxhstudio.pvpstatswotv.dto.ConfrontationCreateDTO;
 import com.mxhstudio.pvpstatswotv.service.ConfrontationService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/confrontations")
@@ -17,8 +20,11 @@ public class ConfrontationController {
         this.confrontationService = confrontationService;
     }
 
-    @PutMapping
-    public Long create(@RequestBody ConfrontationCreateDTO dto){
-        return confrontationService.create(dto);
+    @PutMapping(produces = "application/json")
+    @Operation(summary = "register confrontation", description = "register the confrontation result")
+    public ResponseEntity<Void> create(@RequestBody ConfrontationCreateDTO dto){
+        long id = confrontationService.create(dto);
+        var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/mxhstudio/pvpstatswotv/dto/ConfrontationCreateDTO.java
+++ src/main/java/com/mxhstudio/pvpstatswotv/dto/ConfrontationCreateDTO.java
@@ -1,5 +1,6 @@
 package com.mxhstudio.pvpstatswotv.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,16 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class ConfrontationCreateDTO {
 
+    @Schema(description = "Confrontation date", example = "2021-02-02")
     private LocalDate date;
+    @Schema(description = "total of unit lost in battle", example = "2")
     private Long unitsLost;
+    @Schema(description = "formation id", example = "2")
     private Long formationId;
+    @Schema(description = "Enemy's unit defeated in battle", example = "3")
     private Long unitsDefeated;
+    @Schema(description = "User's id", example = "1")
     private Long userId;
+    @Schema(description = "Season's id", example = "1")
     private Long seasonId;
 }
